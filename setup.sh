bafac77a2b236165b6eeebc9d5fb583224984197
StathisKap <stathiskap75@gmail.com>
2022-09-15 15:41:09 +0300

    Updated Script

--- setup.sh
+++ setup.sh
@@ -1,4 +1,9 @@
+#! /bin/bash
 sudo apt-get install nala
-sudo nala install zsh
-sudo nala install curl
+sudo nala install zsh neovim curl bat zoxide -y
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install
+cp ./vimrc $HOME/.vimrc
+cp ./zshrc $HOME/.zshrc
+cp ./config $HOME/.config
