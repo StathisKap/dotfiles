f0a335d3307774c50636f1f1e64841b8ae7ca664
StathisKap <stathiskap75@gmail.com>
2023-07-22 03:02:38 +0200

    Fixed nala command

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 sudo apt install nala
-sudo nala install zsh neovim curl bat zoxide -y --ignore-missing
+sudo nala install zsh neovim curl bat zoxide -y
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
