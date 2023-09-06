27cbe4c48f89cb8e767e4905e07a57d8b5a7228c
StathisKap <stathiskap75@gmail.com>
2023-09-06 18:06:16 +0100

    Install NodeJS 20.04

--- setup.sh
+++ setup.sh
@@ -13,24 +13,35 @@ fi
 
 OS_TYPE=$(uname -s)
 
+# Mac Install
 if [ "$OS_TYPE" == "Darwin" ]; then
 	sudo brew install zsh neovim curl bat zoxide nodejs exa tldr
 fi
 
+# Linux Install
+if [ "$OS_TYPE" == "Linux" ]; then
+	# Install NodeJS
+	sudo apt-get update
+	sudo apt-get install -y ca-certificates curl gnupg
+	sudo mkdir -p /etc/apt/keyrings
+	curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+	NODE_MAJOR=20
+	echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
+fi
 
+
+# Linux Install 22.04
 if ([ "$OS_TYPE" == "Linux" ] && [ "$(lsb_release -rs)" == "22.04" ]); then
 	sudo apt update && sudo apt upgrade -y
 	sudo apt install nala -y
 	sudo nala install zsh neovim curl bat zoxide nodejs exa tldr -y
 fi
 
+# Linux Install 20.04
 if ([ "$OS_TYPE" == "Linux" ] && [ "$(lsb_release -rs)" != "22.04" ]); then
 	sudo apt update && sudo apt upgrade -y
 	sudo apt install zsh neovim curl bat nodejs tldr -y
 fi
-
-# Latest Node
-# curl -fsSL https://deb.nodesource.com/setup_20.x | sudo -E bash - &&\
 
 # PNPM Install
 curl -fsSL https://get.pnpm.io/install.sh | sh -
